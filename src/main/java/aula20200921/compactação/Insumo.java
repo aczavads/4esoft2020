65c3cf70157b05fc3122d45424848bc91aba4db8
arthur.zavadski <aczavads@gmail.com>
2020-09-21 20:00:34 -0300

    Exemplo de atributo transient

--- src/main/java/aula20200921/compactação/Insumo.java
+++ src/main/java/aula20200921/compactação/Insumo.java
@@ -1,13 +1,23 @@
 package aula20200921.compactação;
 
 import java.io.Serializable;
-import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 
 public class Insumo implements Serializable  {
-    //private static final long serialVersionUID = 3147307285321171386L;
+    private static final long serialVersionUID = -1193117216289434012L;
+    // private static final long serialVersionUID = 3147307285321171386L;
+
+    private transient Logger logger = LoggerFactory.getLogger(Insumo.class);
     private String descricao;
 
     public Insumo(String descricao) {
+        logger.info("Criando um insumo...");
+
+        logger.info("Insumo criado!")
     }
 
 
